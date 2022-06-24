f195d69ffb9498dca05f632d7a10b746c0621958
Guillaume Ojardias <guillaume.ojardias@sketchfab.com>
2022-06-24 16:34:44 +0200

    Split luavim config in multiple files

---
+++ config/lvim/lua/plugins.lua
@@ -0,0 +1,7 @@
+lvim.plugins = {
+  { "echasnovski/mini.nvim" },
+  { "ludovicchabant/vim-gutentags" },
+  { "nvim-telescope/telescope-fzy-native.nvim" },
+  { "folke/trouble.nvim", cmd = "TroubleToggle", },
+  { "Yggdroot/indentLine" }
+}
