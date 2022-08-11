3a47a62c85eb3961cb0d8a629aad741a0a6b3ffb
Guillaume Ojardias <guillaume.ojardias@sketchfab.com>
2022-08-11 10:22:08 +0200

    Updates lsp-settings for showwebgl

--- config/lvim/projects/showwebgl.lua
+++ config/lvim/projects/showwebgl.lua
@@ -1,38 +1,31 @@
-local formatters = require("lvim.lsp.null-ls.formatters")
-
+local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   {
     command = "black",
-    args = {
-      "--config", "/Users/guillaume/dev/sketchfab/pyproject.toml",
+    {
+      args = {
+        "--config", "~/dev/sketchfab/pyproject.toml",
+      },
+      filetype = { "python" }
     },
-    filetype = {
-      "python",
-    }
-  },
-  {
-    command = "isort",
-    args = {
-      "--settings", "/Users/guillaume/dev/sketchfab/pyproject.toml",
-    },
-    filetype = {
-      "python",
+    {
+      command = "isort",
+      args = {
+        "--settings", "~/dev/sketchfab/pyproject.toml",
+      },
+      filetype = { "python" }
     }
   }
 }
-
 lvim.format_on_save = true
 
-local linters = require("lvim.lsp.null-ls.linters")
-
+local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   {
     command = "flake8",
     args = {
-      "--config", "/Users/guillaume/dev/sketchfab/tox.ini",
+      "--config", "~/dev/sketchfab/tox.ini",
     },
-    filetype = {
-      "python",
-    }
+    filetype = { "python" }
   }
 }
