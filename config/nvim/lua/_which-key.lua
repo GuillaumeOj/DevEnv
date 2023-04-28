9c05dfc48071e8307f85851b9d6dbfaf3bc1fca6
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-04-28 11:57:32 +0200

    Adds which_key mapping for reloading current config file

--- config/nvim/lua/_which-key.lua
+++ config/nvim/lua/_which-key.lua
@@ -2,10 +2,15 @@ local wk = require("which-key")
 
 wk.register({
   f = {
-    name = "file",
+    name = "File",
     b = { "<cmd>Telescope buffers<CR>", "List open buffers" },
     f = { "<cmd>Telescope find_files<CR>", "Find file" },
     g = { "<cmd>Telescope git_files<CR>", "Find file within git dir" },
   },
   w = { ":w<CR>", "Save current buffer" },
+  q = { ":q<CR>", "Close current window" },
+  c = {
+    name = "Configuration",
+    r = { ":source %<CR>", "Reload current config file" },
+  }
 }, { prefix = "<leader>" })
