94eef88831c77ffa015b03dd3fae3a7cd6198d9e
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-04-28 11:57:51 +0200

    Adds settings for neovim UI

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,2 +1,19 @@
+local o = vim.o
+local wo = vim.wo
+
+-- Change default <leader> by <space>
 vim.g.mapleader = " "
+
+-- Neovim UI
+-- Colorsheme
 vim.cmd("colorscheme tokyonight")
+
+-- Global options
+o.cmdheight = 4
+
+-- Window-local options
+wo.cursorcolumn = true
+wo.cursorline = true
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = 'yes'
