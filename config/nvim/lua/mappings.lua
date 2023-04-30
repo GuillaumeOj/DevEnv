4d327c78d7a1c1de3fa276b34432f12a63442bd5
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-04-30 18:49:09 +0200

    Removes unused import in mappings

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -1,6 +1,5 @@
 local utils = require("utils")
 local map = utils.map
-local buff_map = utils.buf_map
 
 -- Mappings for Neovim
 map('n', '<esc>', ':nohlsearch<CR>')
@@ -10,4 +9,3 @@ map('n', '<C-h>', '<C-w><C-h>')
 map('n', '<C-j>', '<C-w><C-j>')
 map('n', '<C-k>', '<C-w><C-k>')
 map('n', '<C-l>', '<C-w><C-l>')
-
