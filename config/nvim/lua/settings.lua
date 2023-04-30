c1990577a4b478c36ca777f5cc756437487c89c1
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-04-30 18:44:42 +0200

    Adds undo/redo settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,14 +1,26 @@
+local cmd = vim.cmd
 local o = vim.o
 local wo = vim.wo
 
 -- Change default <leader> by <space>
 vim.g.mapleader = " "
 
--- Neovim UI
 -- Global options
+o.clipboard = 'unnamedplus'
 o.cmdheight = 4
 o.cursorcolumn = true
 o.cursorline = true
 o.number = true
 o.relativenumber = true
 o.signcolumn = 'yes'
+o.undodir = vim.fn.stdpath('config') .. '/undo'
+o.undofile = true
+o.undolevels = 1000
+o.undoreload = 10000
+
+cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
