d2bbae70e94a07e5738dd8e1f5085c999d1cda6a
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-04-30 18:49:42 +0200

    Fixes utils for keymapping

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -6,14 +6,14 @@ local log_levels = vim.log.levels
 -- KeyMapping
 function M.map(mode, lhs, rhs, extra_options)
   local default_options = { noremap = true, silent = true, nowait = false }
-  options = vim.tbl_extend('keep', extra_options or {}, default_options)
+  local options = vim.tbl_extend('keep', extra_options or {}, default_options)
   return api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 function M.buf_map(buffnr, mode, lhs, rhs, extra_options)
   local default_options = { noremap = true, silent = true }
-  options = vim.tbl_extend('keep', extra_options or {}, default_options)
-  return api.nvim_buf_set_keymap(buffnbr, mode, lhs, rhs, options)
+  local options = vim.tbl_extend('keep', extra_options or {}, default_options)
+  return api.nvim_buf_set_keymap(buffnr, mode, lhs, rhs, options)
 end
 
 -- Notifications
