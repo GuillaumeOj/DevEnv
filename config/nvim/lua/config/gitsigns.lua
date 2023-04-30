7ba13696ce78bfe80ce9c4f8d78b2eba303ce0a2
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-05-01 00:21:06 +0200

    Adds blame on current line

---
+++ config/nvim/lua/config/gitsigns.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.setup()
+  require('gitsigns').setup({
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 100,
+    },
+  })
+end
+
+return M
