5864c45c80838c87e963c580b7a49c9af00c9b04
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2024-02-16 16:36:32 +0100

    chore: updates LspRestart after retrieving venv from cache

--- config/nvim/lua/configs/venv-selector.lua
+++ config/nvim/lua/configs/venv-selector.lua
@@ -6,6 +6,9 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
 		if venv ~= "" then
 			require("venv-selector").retrieve_from_cache()
+			vim.defer_fn(function()
+				vim.cmd("LspRestart")
+			end, 2000)
 		end
 	end,
 	once = true,
