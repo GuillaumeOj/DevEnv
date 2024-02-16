efd1114468f56379bd1e0a2c939804a2b95aae6d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2024-02-16 11:09:15 +0100

    feat: adds settings for undo files and buffer settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -7,7 +7,13 @@ vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
+vim.o.scrolljump = 10
+vim.o.scrolloff = 10
 vim.o.swapfile = false
+vim.o.undodir = vim.fn.stdpath("config") .. "/undo"
+vim.o.undofile = true
+vim.o.undolevels = 1000
+vim.o.undoreload = 10000
 vim.g.python3_host_prog = "/Users/guillaume/.pyenv/shims/python"
 
 vim.cmd([[autocmd BufWritePost * silent! FormatWrite]])
